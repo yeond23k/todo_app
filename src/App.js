21f7c9d1f54196ba9596578d81b65074069845fc
dyk <dyk@dykui-MacBook-Air.local>
2021-03-15 23:47:54 +0900

    setup antd

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Row, Col, Button, DatePicker } from 'antd';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Row>
+        <h3>TODO LIST APP</h3>
+      </Row>
+      <Row>
+        <Col>
+          <Button>TODO LIST</Button>
+        </Col>
+      </Row>
     </div>
   );
-}
+};
 
 export default App;
